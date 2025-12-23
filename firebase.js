8b863c6a950c7b0698350b86ec5efad9601a452c
stbinske-ship-it <stbinske@gmail.com>
2025-12-23 12:37:02 +0100

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,9 +1,9 @@
 // FIREBASE CONFIG HIER EINFÜGEN
 const firebaseConfig = {
-  apiKey: "DEIN_API_KEY",
-  authDomain: "DEIN_AUTH_DOMAIN",
-  databaseURL: "DEINE_DATABASE_URL",
-  projectId: "DEIN_PROJECT_ID"
+  apiKey: "AIzaSyC_Uhhm0w-UZam1LctPMnGcxZkXPSjx_8g",
+  authDomain: "wizzard-pwa.firebaseapp.com",
+  databaseURL: "https://wizzard-pwa-default-rtdb.firebaseio.com",
+  projectId: "wizzard-pwa"
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
